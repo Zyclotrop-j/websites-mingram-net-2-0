637a7ab4a45f8e8747cde4c322e0e671ea13b4b0
Zyc <zyclotrop.j@gmail.com>
2022-07-12 15:37:13 +0000

    modify permissions to allow for projects

--- utils/AdminComponent.tsx
+++ utils/AdminComponent.tsx
@@ -9,6 +9,9 @@ const dynamicResources = async (permissions) => {
   console.log(permissions);
   const user = await authProvider.checkAuth();
   console.log(user);
+  // in the future, we can calculate the current project here!
+  // in firebase, under /projects/{projectid} -> users: [u1, u2, u3] is an allow-list function
+  // resource def could be users/${projectid}/pages, if user is in project
   return [
     <Resource name={`users/${user.uid}/pages`} options={{label: "pages"}} {...pages} />,
     <Resource name={`users/${user.uid}/products`} options={{label: "product"}} {...products} />
